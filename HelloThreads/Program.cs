3ad3c6320a857103f6f9213a0419a5c7cbd4ec04
thelittlecitizen13 <thelittlecitizen13@gmail.com>
2020-08-24 20:41:17 +0300

    Print1000Times function created to contain the first question code

--- HelloThreads/Program.cs
+++ HelloThreads/Program.cs
@@ -7,10 +7,7 @@ namespace HelloThreads
     {
         static void Main(string[] args)
         {
-            Thread t1 = new Thread(() => PrintWord("mamas", 1000));
-            Thread t2 = new Thread(() => PrintWord("Empire", 1000));
-            t1.Start();
-            t2.Start();
+            
         }
         public static void PrintWord(string word, int times)
         {
@@ -18,6 +15,16 @@ namespace HelloThreads
             {
                 Console.WriteLine("The word is: " + word);
             }
+        }
+
+        static void Print1000times()
+        {
+            Thread t1 = new Thread(() => PrintWord("mamas", 1000));
+            Thread t2 = new Thread(() => PrintWord("Empire", 1000));
+            t1.IsBackground = true;
+            t2.IsBackground = true;
+            t1.Start();
+            t2.Start();
         }
     }
 }
