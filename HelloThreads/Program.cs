03b7281b9195d1fac090eaa4fb8774bbe799176c
thelittlecitizen13 <thelittlecitizen13@gmail.com>
2020-08-24 20:35:21 +0300

    Two threads prints 100 times mamas & empire

---
+++ HelloThreads/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+
+namespace HelloThreads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(() => PrintWord("mamas", 1000));
+            Thread t2 = new Thread(() => PrintWord("Empire", 1000));
+            t1.Start();
+            t2.Start();
+        }
+        public static void PrintWord(string word, int times)
+        {
+            for (int i = 0; i < times; i++)
+            {
+                Console.WriteLine("The word is: " + word);
+            }
+        }
+    }
+}
